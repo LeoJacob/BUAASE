4e66f8e136bdea9901d972f8ae991684a5ec5e23
Leo Jacob <lzy4575@126.com>
2017-09-24 03:53:03 +0800

    初步整合生成数独功能并优化了速度
    加入输出优化后使生成 100w 个数独的速度从 16s 变成 5.5s。

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -2,10 +2,53 @@
 //
 
 #include "stdafx.h"
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include "sudoku9.h"
+#include "generator.h"
+#include "solver.h"
 
+using namespace std;
 
-int main()
+void outputNum(FILE *&fout, int x)
 {
+	if (x >= 10)
+		outputNum(fout, x / 10);
+	fputc('0' + x % 10, fout);
+}
+
+int main(int argc, char *argv[])
+{
+	int T;
+	int t, i, j;
+	Sudoku9 puzzle;
+	NaiveSudoku9Generator generator;
+	FILE *fout;
+	fout = fopen("sudoku.txt", "w");
+	//scanf("%d", &T);
+	T = 1000000;
+	for (t = 0; t < T; t += 1)
+	{
+		puzzle = generator.generate();
+		for (i = 0; i < 9; i += 1)
+		{
+			for (j = 0; j < 9; j += 1)
+			{
+				outputNum(fout, puzzle.data[i][j]);
+				fputc(" \n"[j + 1 == 9 ? 1 : 0], fout);
+				//fprintf(fout, "%d%c", puzzle.data[i][j], " \n"[j + 1 == 9 ? 1 : 0]);
+			}
+		}
+		if (t + 1 < T)
+		{
+			fputc('\n', fout);
+			//fprintf(fout, "\n");
+		}
+	}
+	fclose(fout);
     return 0;
 }
 
