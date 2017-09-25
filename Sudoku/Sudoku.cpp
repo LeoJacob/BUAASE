235c82293bb2a6c17e9378c547cb9f9bff1a8bf6
Leo Jacob <lzy4575@126.com>
2017-09-26 07:49:45 +0800

    完善了命令行处理部分

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -22,33 +22,103 @@ void outputNum(FILE *&fout, int x)
 
 int main(int argc, char *argv[])
 {
-	int T;
-	int t, i, j;
-	Sudoku9 puzzle;
-	NaiveSudoku9Generator generator;
-	FILE *fout;
-	fout = fopen("sudoku.txt", "w");
-	//scanf("%d", &T);
-	T = 1000000;
-	for (t = 0; t < T; t += 1)
+	if (argc != 3
+		|| strlen(argv[1]) != 2
+		|| argv[1][0] != '-'
+		|| (argv[1][1] != 'c' && argv[1][1] != 's'))
+		goto WRONG_PARAMETERS;
+
+	if (argv[1][1] == 's')
 	{
-		puzzle = generator.generate();
-		for (i = 0; i < 9; i += 1)
+		int i, j;
+		int cnt = 0;
+		Sudoku9DLXSolver solver;
+		Sudoku9 puzzle, ans;
+		char buffer[200] = {};
+		FILE *fout;
+		if (!freopen(argv[2], "r", stdin))
+			goto WRONG_PARAMETERS;
+		fout = fopen("sudoku.txt", "w");
+		while (1)
 		{
-			for (j = 0; j < 9; j += 1)
+			for (i = 0; i < 9; i += 1)
 			{
-				outputNum(fout, puzzle.data[i][j]);
-				fputc(" \n"[j + 1 == 9 ? 1 : 0], fout);
-				//fprintf(fout, "%d%c", puzzle.data[i][j], " \n"[j + 1 == 9 ? 1 : 0]);
+				for (j = 0; j < 9; j += 1)
+				{
+					if (scanf("%d", &puzzle.data[i][j]) == EOF)
+						break;
+				}
+				if (j < 9)
+					break;
 			}
+			if (i < 9)
+				break;
+			if (cnt)
+				fputc('\n', fout);
+			solver.set(puzzle);
+			if (solver.solve())
+			{
+				ans = solver.solution();
+				for (i = 0; i < 9; i += 1)
+				{
+					for (j = 0; j < 9; j += 1)
+					{
+						buffer[(i * 9 + j) * 2] = ans.data[i][j] + '0';
+						buffer[(i * 9 + j) * 2 + 1] = " \n"[j + 1 == 9 ? 1 : 0];
+					}
+				}
+				fwrite(buffer, 1, 162, fout);
+			}
+			else
+				fprintf(fout, "No Solution!\n");
+			cnt += 1;
 		}
-		if (t + 1 < T)
-		{
-			fputc('\n', fout);
-			//fprintf(fout, "\n");
-		}
+		fclose(fout);
 	}
-	fclose(fout);
-    return 0;
+	else if (argv[1][1] == 'c')
+	{
+		if (strlen(argv[2]) > 7)
+			goto WRONG_PARAMETERS;
+
+		int T = 0;
+		int t, i, j;
+		for(i = 0;argv[2][i];i += 1)
+		{
+			if (!isdigit(argv[2][i]))
+				goto WRONG_PARAMETERS;
+			T = T * 10 + argv[2][i] - '0';
+		}
+		if (T > 1000000)
+			goto WRONG_PARAMETERS;
+
+		Sudoku9 puzzle;
+		NaiveSudoku9Generator generator;
+		FILE *fout;
+		char buffer[200] = {};
+		fout = fopen("sudoku.txt", "w");
+		for (t = 0; t < T; t += 1)
+		{
+			puzzle = generator.generate();
+			for (i = 0; i < 9; i += 1)
+			{
+				for (j = 0; j < 9; j += 1)
+				{
+					buffer[(i * 9 + j) * 2] = puzzle.data[i][j] + '0';
+					buffer[(i * 9 + j) * 2 + 1] = " \n"[j + 1 == 9 ? 1 : 0];
+				}
+			}
+			fwrite(buffer, 1, 162, fout);
+			if (t + 1 < T)
+				fputc('\n', fout);
+		}
+		fclose(fout);
+	}
+
+EXIT:
+	return 0;
+
+WRONG_PARAMETERS:
+	printf("Wrong Parameters!\n");
+	goto EXIT;
 }
 
