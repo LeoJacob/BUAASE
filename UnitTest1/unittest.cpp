ae8469692d25f440edb16be309603b1dff81a85e
Leo Jacob <lzy4575@126.com>
2017-09-26 20:49:55 +0800

    添加了若干单元测试

--- UnitTest1/unittest.cpp
+++ UnitTest1/unittest.cpp
@@ -12,7 +12,7 @@ namespace UnitTest
 	{
 	public:
 		
-		TEST_METHOD(HasSolution)
+		TEST_METHOD(HasSolution1)
 		{
 			Sudoku9 puzzle(
 				"800000000"
@@ -40,7 +40,35 @@ namespace UnitTest
 			}
 		}
 
-		TEST_METHOD(NoSolution)
+		TEST_METHOD(HasSolution2)
+		{
+			Sudoku9 puzzle(
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"), origin, ans;
+			origin = puzzle;
+			Sudoku9DLXSolver solver(puzzle);
+			Assert::IsTrue(solver.solve());
+			ans = solver.solution();
+			Assert::IsTrue(ans.isValid());
+			int i, j;
+			for (i = 0; i < 9; i += 1)
+			{
+				for (j = 0; j < 9; j += 1)
+				{
+					if (origin.data[i][j] > 0)
+						Assert::IsTrue(origin.data[i][j] == ans.data[i][j]);
+				}
+			}
+		}
+
+		TEST_METHOD(NoSolution1)
 		{
 			Sudoku9 puzzle(
 				"801000000"
@@ -52,6 +80,54 @@ namespace UnitTest
 				"001000068"
 				"008500010"
 				"090000400");
+			Sudoku9DLXSolver solver(puzzle);
+			Assert::IsFalse(solver.solve());
+		}
+
+		TEST_METHOD(NoSolution2)
+		{
+			Sudoku9 puzzle(
+				"800000000"
+				"000000000"
+				"008000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000");
+			Sudoku9DLXSolver solver(puzzle);
+			Assert::IsFalse(solver.solve());
+		}
+
+		TEST_METHOD(NoSolution3)
+		{
+			Sudoku9 puzzle(
+				"800000008"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000");
+			Sudoku9DLXSolver solver(puzzle);
+			Assert::IsFalse(solver.solve());
+		}
+
+		TEST_METHOD(NoSolution4)
+		{
+			Sudoku9 puzzle(
+				"800000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"000000000"
+				"800000000");
 			Sudoku9DLXSolver solver(puzzle);
 			Assert::IsFalse(solver.solve());
 		}
@@ -95,6 +171,28 @@ namespace UnitTest
 			ones.push_back(make_pair(5, 6));
 			DLXSolver solver(ones, 7);
 			Assert::IsTrue(solver.solve());
+		}
+
+		TEST_METHOD(GenerateValidPuzzle)
+		{
+			NaiveSudoku9Generator generator;
+			Sudoku9 puzzle;
+			puzzle = generator.generate();
+			Assert::IsTrue(puzzle.isValid());
+			Assert::IsTrue(puzzle.data[0][0] == (0 + 6) % 9 + 1);
+		}
+
+		TEST_METHOD(Generate1000000ValidPuzzle)
+		{
+			int T = 1000000;
+			NaiveSudoku9Generator generator;
+			Sudoku9 puzzle;
+			for(int t = 0;t < T;t += 1)
+			{
+				puzzle = generator.generate();
+				Assert::IsTrue(puzzle.isValid());
+				Assert::IsTrue(puzzle.data[0][0] == (0 + 6) % 9 + 1);
+			}
 		}
 
 	};
