3980d3c2de4866e7aacfcc99e0dedd81faabacee
fkd151136 <fangkedong@buaa.edu.cn>
2017-10-08 00:13:45 +0800

    初步实现生成数独请求页面

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "SudokuUI.h"
+#include "getGenetateNumber.h"
 #include <QtWidgets/QApplication>
+
+int iGenerateNumber = 0;
+int iMode = 1;
+int iMinSpace = 20;
+int iMaxSpace = 20;
+bool bUnique = false;
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	getGenetateNumber getNumber(iGenerateNumber, iMode, iMinSpace, iMaxSpace, bUnique);
+	a.setActiveWindow(&getNumber);
+	getNumber.show();
 	SudokuUI w;
-	w.show();
+	//w.show();
 	return a.exec();
 }
