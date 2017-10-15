367d7f4416ccd5a8ba92f9f13b48cf20e6c67e70
fkd151136 <fangkedong@buaa.edu.cn>
2017-10-15 11:04:52 +0800

    再次优化UI，计算模块加速

--- Core.h
+++ Core.h
@@ -13,6 +13,5 @@ public:
     void generate(int number, int mode, int result[][81]);
     void generate(int number, int lower, int upper, bool unique, int result[][81]);
     bool solve(int puzzle[81], int solution[81]);
-	void testLink();
 };
 
